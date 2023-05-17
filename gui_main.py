2a2b3e7b63ce095242a91496f2b6cb9dcec3d11f
Mindaugas Turauskas <min.turauskas@gmail.com>
2023-05-17 13:22:30 +0300

    GUI

--- gui_main.py
+++ gui_main.py
@@ -1,119 +1,91 @@
-from attendance import *
-from sqlalchemy.orm import sessionmaker
-from datetime import datetime
 import PySimpleGUI as sg
+from datetime import date
 
-Session = sessionmaker(bind=engine)
-session = Session()
-headings=['Lecture', 'Student', 'Attendance']
-layout = [
-    [sg.Text(headings[0], size=(15,1)), sg.Input(size=20,key='-NAME-')],
-    [sg.Text(headings[1], size=(15,1)), sg.Input(size=20,key='-SURNAME-')],
-    [sg.Text(headings[2], size=(15,1)), sg.Input(size=20,key='-BIRTHDATE-')],
-    [sg.Button('Add', key='-ADD-'), sg.Button('View', key='-VIEWLIST-'), sg.Button('Edit', key='-CHANGELIST-'), sg.Button('Save', key='-SAVE-', disabled=True), sg.Button('Delete', key='-DELETE-')],
-    [sg.Table(values=[], headings=['ID', 'Lecture', 'Student', 'Attendance'], key='-EMPLOYEES-', justification='left', auto_size_columns=False, col_widths=[3, 10, 10, 12, 17, 10, 15])]
-]
 
-window = sg.Window('Students attendance in a lecture', layout)
+# darbuotojai = PersonaloValdymas()
+# darbuotojai = PersonaloValdymas.pickle_nuskaitymas(darbuotojai)
+td=[]
+headings=['Vardas Pavardė', 'Komentaras', 'Alga', 'Priimtas', 'Telefono numeris', 'Asmens kodas', 'Išsilavinimas', 'Skyrius']
+
+layout=[
+        [sg.Text(headings[0], size=(15,1)),sg.Input(size=20,key=headings[0])],
+        [sg.Text(headings[1], size=(15,1)),sg.Input(size=20,key=headings[1])],
+        [sg.Text(headings[2], size=(15,1)),sg.Input(size=20,key=headings[2])],
+        [sg.Text(headings[3], size=(15,1)),sg.Input(size=20,key=headings[3])],
+        [sg.Text(headings[4], size=(15,1)),sg.Input(size=20,key=headings[4])],
+        [sg.Text(headings[5], size=(15,1)),sg.Input(size=20,key=headings[5])],
+        [sg.Text(headings[6], size=(15,1)),sg.Input(size=20,key=headings[6])],
+        [sg.Text(headings[7], size=(18,1)),sg.Combo(['IT skyrius','Finansai','Administracija'],key=headings[7])],
+        [sg.Button('Pridėti'), sg.Button('Redaguoti'), sg.Button('Išsaugoti',disabled=True), sg.Button('Ištrinti'), sg.Push(), sg.Exit()],
+        [sg.Table(td,headings,key='myTable')]]
+
+window=sg.Window('Personalo valdymo programa',layout)
+# 
 while True:
-   event, values = window.read()
-   print(event, values)
-   if event in (None, 'Exit'):
-      break
+    event,values= window.read()
+    print (values)
+    if event == 'Pridėti':
+        values = [values[h] for h in headings]
+        td.append(values)
+        darbuotojai.prideti_darbuotoja(*values)
+        darbuotojai.darbuotoju_sarasas()
+        window['myTable'].update(values=td)
+        for i in range(len(headings)):    
+            window[headings[i]].update(value='')
+    if event == 'Redaguoti':
+        if values['myTable']==[]:
+            sg.popup('Pasirinkite eilutę, kurią norite redaguoti')
+        else:
+            editRow=values['myTable'][0]
+            for i in range(len(headings)):  
+                window[headings[i]].update(value=td[editRow][i])
+            window['Išsaugoti'].update(disabled=False)
+    if event == 'Išsaugoti':
+        td[editRow]=([values[h] for h in headings])
+        window['myTable'].update(values=td)
+        for i in range(len(headings)):
+            window[headings[i]].update(value='')
+        window['Išsaugoti'].update(disabled=True)
+    if event == 'Ištrinti':
+        if values['myTable']==[]:
+            sg.popup('Pasirinkite eilutę')
+        else:
+            if sg.popup_ok_cancel('Ar tikrai norite tęsti?') == 'OK':
+                pasirinkta_eilute = values['myTable'][0]
+                pasirinktas_vardas = td[pasirinkta_eilute][0]
+                print(pasirinktas_vardas)
+                del td[pasirinkta_eilute]
+                data = date.today()
+                data_str = data.strftime('%Y-%m-%d')
+                darbuotojai.atleisti_darbuotoja(pasirinktas_vardas, data_str)
+                darbuotojai.atleistu_darbuotoju_sarasas()
+                darbuotojai.darbuotoju_sarasas()
+                window['myTable'].update(values=td)
+    if event in (sg.WIN_CLOSED, 'Exit'):
+        break
 window.close()
 
-# 
-# 
-def create_employee(name, surname, birthdate, position, salary):
-    birthdate = datetime.strptime(birthdate, '%Y-%m-%d').date()
-    employee = Employee(name=name, surname=surname, birthdate=birthdate, position=position, salary=salary)
-    session.add(employee)
-    session.commit()
 
-def get_employees():
-    employees = session.query(Employee).all()
-    return employees
+# from attendance import *
+# from sqlalchemy.orm import sessionmaker
+# from datetime import datetime
+# import PySimpleGUI as sg
 
-def change_employee(employee_id, **kwargs):
-    employee = session.query(Employee).filter(Employee.id == employee_id).first()
-    if employee:
-        for key, value in kwargs.items():
-            if hasattr(employee, key):
-                setattr(employee, key, value)
-        session.commit()
+# Session = sessionmaker(bind=engine)
+# session = Session()
+# headings=['Lecture', 'Student', 'Attendance']
+# layout = [
+#     [sg.Text(headings[0], size=(15,1)), sg.Input(size=20,key='-NAME-')],
+#     [sg.Text(headings[1], size=(15,1)), sg.Input(size=20,key='-SURNAME-')],
+#     [sg.Text(headings[2], size=(15,1)), sg.Input(size=20,key='-BIRTHDATE-')],
+#     [sg.Button('Add', key='-ADD-'), sg.Button('View', key='-VIEWLIST-'), sg.Button('Edit', key='-CHANGELIST-'), sg.Button('Save', key='-SAVE-', disabled=True), sg.Button('Delete', key='-DELETE-')],
+#     [sg.Table(values=[], headings=['ID', 'Lecture', 'Student', 'Attendance'], key='-EMPLOYEES-', justification='left', auto_size_columns=False, col_widths=[3, 10, 10, 12, 17, 10, 15])]
+# ]
 
-def delete_employee(employee_id):
-    employee = session.query(Employee).filter(Employee.id == employee_id).first()
-    if employee:
-        session.delete(employee)
-        session.commit()
-
-def employee_list(window):
-    employees = get_employees()
-    window['-EMPLOYEES-'].update([f'{e.id} {e.name} {e.surname} {e.birthdate} {e.position} {e.salary} {e.working_since}' for e in employees])
-
-def delete_selected_employee(window, values):
-    selected_rows = values['-EMPLOYEES-']
-    if selected_rows:
-        selected_row_index = selected_rows[0]
-        employees = get_employees()
-        if selected_row_index < len(employees):
-            employee = employees[selected_row_index]
-            employee_id = employee.id
-            delete_employee(employee_id)
-            sg.popup('Employee deleted succesfully')
-    employee_list(window)
-
-def save_updated_employee(window, employee_id, values):
-    if employee_id is not None:
-        name = values['-NAME-']
-        surname = values['-SURNAME-']
-        birthdate = values['-BIRTHDATE-']
-        position = values['-POSITION-']
-        salary = values['-SALARY-']
-        if name or surname or birthdate or position or salary:
-            try:
-                birthdate = datetime.strptime(birthdate, '%Y-%m-%d').date()
-                change_employee(employee_id, name=name, surname=surname, birthdate=birthdate, position=position, salary=salary)
-                employee_list(window)
-            except ValueError:
-                sg.popup('Incorrect date format, please use this format: YYYY-MM-DD.')
-
-def employee_changelist(window, values):
-    sg.popup('Enter new values you wish to change')
-    selected_rows = values['-EMPLOYEES-']
-    if selected_rows:
-        selected_row_index = selected_rows[0]
-        employees = get_employees()
-        if selected_row_index < len(employees):
-            employee = employees[selected_row_index]
-            for attribute, value in zip(['-NAME-', '-SURNAME-', '-BIRTHDATE-', '-POSITION-', '-SALARY-'], [employee.name, employee.surname, employee.birthdate, employee.position, employee.salary]):
-                window[attribute].update(value=value)
-            window['-SAVE-'].update(disabled=False)
-        else:
-            sg.popup('Invalid selected row index.')
-    else:
-        sg.popup('Please select an employee from the list.') 
-
-def get_index(values):
-    selected_rows = values['-EMPLOYEES-']
-    if selected_rows:
-        selected_row_index = selected_rows[0]
-        employees = get_employees()
-        if selected_row_index < len(employees):
-            employee = employees[selected_row_index]
-            employee_id = employee.id   
-    return employee_id
-
-def add_employee(window, values):
-    keys=['-NAME-', '-SURNAME-', '-BIRTHDATE-', '-POSITION-', '-SALARY-']
-    input_values = [values[key] for key in keys]
-    if all(input_values):
-        try:
-            create_employee(input_values[0], input_values[1], input_values[2], input_values[3], input_values[4])
-            sg.popup('Employee succesfully added!')
-        except ValueError:
-            sg.popup('Incorrect date format, please use this format: YYYY-MM-DD.')
-    else:
-        sg.popup('Please fill all the fields.')
-    employee_list(window)
+# window = sg.Window('Students attendance in a lecture', layout)
+# while True:
+#    event, values = window.read()
+#    print(event, values)
+#    if event in (None, 'Exit'):
+#       break
+# window.close()
